32936935b57262853d582ec1d86e5957d4d783a4
TIMITE Ahmed <ahmed.timite@gmail.com>
2026-06-24 13:46:36 +0000

    Fix missing useAuthStore import in Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useAuthStore from "../store/authStore";
 import { UserCircleIcon } from "../components/Icon";
 
 export default function Login() {
